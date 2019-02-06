cb260a1def1e61eba3e20ef8930297c4262f5915
kasantos2 <kasantos2@up.edu.ph>
2019-02-06 23:29:18 +0800

    update files

--- sossf-ui/homepage.php
+++ sossf-ui/homepage.php
@@ -1,6 +1,4 @@
-<?php
-  include_once ('server.php')
-?>
+<?php include_once('server.php'); ?>
 
 <!DOCTYPE html>
 <html>
@@ -10,13 +8,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100|Montserrat|Maven+Pro|Nunito+Sans|Changa+One|Questrial" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
     <!-------------------------------- HEADER -------------------------------->
     <header>
-      <img src="logo.png" height="50px" style="margin-top: 30px; color: #fff">
-      
+      <img src="logo.png" height="50px" style="margin-top: 30px; color: #fff">  
       <div class="searchbar"><form action="search-results.php" method="post">
-		   <button type="submit"><i class="fa fa-search"></i></button>
-		   <input type="text" placeholder="Search..." name="input" value="">
+       <button type="submit"><i class="fa fa-search"></i></button>
+       <input type="text" placeholder="Search..." name="input" value="">
        <div class="dropdown">
         <button class="dropbtn"><i class="material-icons">person</i></button>
           <div class="dropdown-content">
@@ -24,14 +22,15 @@
             <a href="#">Link 2</a>
             <a href="#">Sign Out</a>
           </div>
-      	</div>
-		</div>
-    <a href="viewall-admin.php" class="viewall">View All</a>
+        </div>
+      </div>
+      <a href="viewall-admin.php" class="viewall">View All</a>
     </header>
     <!------------------------------------------------------------------------->
+
   </head>
-  <body>
-    
+
+  <body>  
     <div class="content" style="height: 100%">
     <!-- PUT HOMEPAGE HANASH HERE -->
     </div>
@@ -40,10 +39,7 @@
 <script type="text/javascript" src="add-establishment.js">
 
 </script>
+
 </html>
 
-<?php
-
-$connection->close();
-?>
-
+<?php $connection->close(); ?>
--- sossf-ui/viewall-admin.php
+++ sossf-ui/viewall-admin.php
@@ -1,6 +1,4 @@
-<?php
-  include_once ('server.php')
-?>
+<?php include_once ('server.php'); ?>
 
 <!DOCTYPE html>
 <html>
