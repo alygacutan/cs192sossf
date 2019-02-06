c9e2abbca7bf7358d41b67e46b0d553fd0ddbf62
alygacutan <44958734+alygacutan@users.noreply.github.com>
2019-02-07 04:18:43 +0800

    Add files via upload

---
+++ sossf-ui/header.php
@@ -0,0 +1,19 @@
+
+    <!-------------------------------- HEADER -------------------------------->
+    <header>
+      <img src="logo.png" height="50px" style="margin-top: 30px; color: #fff">
+      
+      <div class="searchbar"><form action="search-results.php" method="post">
+		   <button type="submit"><i class="fa fa-search"></i></button>
+		   <input type="text" placeholder="Search..." name="input" value="">
+       <div class="dropdown">
+        <button class="dropbtn"><i class="material-icons">person</i></button>
+          <div class="dropdown-content">
+            <a href="#">Profile</a>
+            <a href="#">Link 2</a>
+            <a href="#">Sign Out</a>
+          </div>
+      	</div>
+		</div>
+      <a href="viewall-admin.php" class="viewall">View All</a>
+    </header>
--- sossf-ui/homepage.php
+++ sossf-ui/homepage.php
@@ -1,45 +1,46 @@
-<?php include_once('server.php'); ?>
+<?php
+  include_once ('server.php');
+  include_once ('header.php');
+  include_once ('extras.php');
+  include_once ('add-establishment.php') 
+?>
 
 <!DOCTYPE html>
 <html>
   <head>
-    <base target="_top">
-    <link rel="stylesheet" type="text/css" href="sossf-stylesheet.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100|Montserrat|Maven+Pro|Nunito+Sans|Changa+One|Questrial" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    
-    <!-------------------------------- HEADER -------------------------------->
-    <header>
-      <img src="logo.png" height="50px" style="margin-top: 30px; color: #fff">  
-      <div class="searchbar"><form action="search-results.php" method="post">
-       <button type="submit"><i class="fa fa-search"></i></button>
-       <input type="text" placeholder="Search..." name="input" value="">
-       <div class="dropdown">
-        <button class="dropbtn"><i class="material-icons">person</i></button>
-          <div class="dropdown-content">
-            <a href="#">Profile</a>
-            <a href="#">Link 2</a>
-            <a href="#">Sign Out</a>
-          </div>
-        </div>
-      </div>
-      <a href="viewall-admin.php" class="viewall">View All</a>
-    </header>
-    <!------------------------------------------------------------------------->
-
   </head>
-
-  <body>  
-    <div class="content" style="height: 100%">
-    <!-- PUT HOMEPAGE HANASH HERE -->
+  <body>
+    
+    <div class="content"><center><br><br>
+      <h2>What are you looking for?</h2>
+      <div class="searchbarhomepage">
+        <form action="search-results.php" method="post">
+          <input type="text" placeholder="Name" name="name" value="">
+          <input type="text" placeholder="Location" name="location" value="">
+          <select name="tags">
+            <option value="">-Select Type-</option>
+            <option value="print">Print</option>
+            <option value="photocopy">Photocopy</option>
+            <option value="binding">Binding</option>
+            <option value="internet">Internet</option>
+            <option value="supplies">Supplies</option>
+            <option value="drugstore">Drugstore</option>
+            <option value="grocery">Grocery</option>
+            <option value="cleaning">Cleaning</option>
+          </select>
+          <button type="submit"><i class="fa fa-search"></i></button>
+      </div>
+    </center>
     </div>
   </body>
 
 <script type="text/javascript" src="add-establishment.js">
 
 </script>
-
 </html>
 
-<?php $connection->close(); ?>
+<?php
+
+$connection->close();
+?>
+
