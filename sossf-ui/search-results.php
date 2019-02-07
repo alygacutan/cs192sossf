6efff5737d6b70a6d6b94d7d4d05669e75a31dc5
kasantos2 <kasantos2@up.edu.ph>
2019-02-07 15:53:29 +0800

    update php files

--- sossf-ui/search-results.php
+++ sossf-ui/search-results.php
@@ -1,35 +1,34 @@
 <?php
-  include_once ('server.php');
-  include_once ('header.php');
-  include_once ('extras.php');
-  include_once ('add-establishment.php') 
+	include_once ('server.php');
+	include_once ('header.php');
+	include_once ('extras.php');
+	include_once ('add-establishment.php') 
 ?>
-
 
 <!DOCTYPE html>
 <html>
-  <head>
-   
-  </head>
-  <body>
-    
-    <div class="content">
-    <h1>Search Results:</h1>
 
-    <!-- ADD ESTABLISHMENT POPUP WINDOW -->
-    <button id="myBtn"> ADD ESTABLISHMENT </button>
-    
-    </div>
-  </body>
+	<head>
+	</head>
 
-<script type="text/javascript" src="add-establishment.js">
+	<body>		
+		<div class="content">
+		<h1>Search Results:</h1>
 
+		<!-- ADD ESTABLISHMENT POPUP WINDOW -->
+		<button id="myBtn"> ADD ESTABLISHMENT </button>
+		
+		<h2>WORK IN PROGRESS</h2>
+		
+		</div>
+	</body>
+
+<script
+type="text/javascript" src="add-establishment.js">
 </script>
+
 </html>
 
-<?php
-
-$connection->close();
-?>
+<?php $connection->close(); ?>
 
 
