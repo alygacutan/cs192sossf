cbbe9c573e3dcae04b14c7cd224b36de8024f182
mauvekeith <47022312+mauvekeith@users.noreply.github.com>
2019-02-08 13:23:58 +0800

    Update 192db.sql

--- 192db.sql
+++ 192db.sql
@@ -1,3 +1,16 @@
+/*
+Code History
+v1.0 - Jan 28, 2019 - Initial file with CREATE TABLE statements [Daine Daling]
+v1.1 - Jan 29, 2019 - Added INSERT INTO statements [Daine Daling]
+*/
+
+/*
+File Creation Date: Jan 28, 2019
+Development Group: SOSSF Group 
+Client Group: N/A
+Purpose: This file creates and partially populates the database for the SOSSF project
+*/
+
 CREATE TABLE Student (
   username VARCHAR(100) NOT NULL,
   name VARCHAR(100) NOT NULL,
@@ -42,5 +55,5 @@ INSERT INTO Student VALUES("student_test1","student1","student1@test.com","testp
 INSERT INTO EstablishmentRepresentative VALUES("estrep_test1","estrep1","estrep1@test.com","testpass","");
 INSERT INTO EstablishmentRepresentative VALUES("estrep_test2","estrep2","estrep2@test.com","testpass","");
 INSERT INTO Admin VALUES(1,"daine","dainedaling@gmail.com","password");
-INSERT INTO Establishment VALUES(1,"est_test1","UP Diliman","printing, photocopy","8am to 5pm","09995551234","",1,"estrep_test1");
-INSERT INTO Establishment VALUES(2,"est_test2","UP Village","books","9am to 7pm","09154444321","",0,"estrep_test2");
+INSERT INTO Establishment VALUES(1,"est_test1","est1","UP Diliman","printing, photocopy","8am to 5pm","09995551234","","1","estrep_test1");
+INSERT INTO Establishment VALUES(2,"est_test2","est2","UP Village","books","9am to 7pm","09154444321","","0","estrep_test2");
