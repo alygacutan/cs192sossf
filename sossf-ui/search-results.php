91a4dac33fbe21628a9d8d641f1e9e53334f6d69
alygacutan <44958734+alygacutan@users.noreply.github.com>
2019-02-08 15:27:45 +0800

    Added code history

--- sossf-ui/search-results.php
+++ sossf-ui/search-results.php
@@ -1,3 +1,17 @@
+<!-- 
+Code History
+v1.0 - Feb 06, 2019 - Initial file - HTML [Aly Gacutan]
+v2.0 - Feb 07, 2019 - Added PHP code [Kenneth Santos]
+v2.1 - Feb 07, 2019 - Revised HTML code, minor changes [Aly Gacutan]
+v2.2 - Feb 07, 2019 - Revised PHP code [Kenneth Santos]
+v3.0 - Feb 08, 2019 - Revised HTML code, minor changes [Aly Gacutan]
+v3.1 - Feb 08, 2019 - Revised PHP code [Kenneth Santos]
+
+File Creation Date: Feb 06,2019
+Development Group: SOSSF Group 
+Client Group: N/A
+Purpose: The HTML/PHP File for Search Page.
+ -->
 <?php
   include_once ('server.php');
   include_once ('header.php');
