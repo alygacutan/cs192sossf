c7b5285afd69deadb25fc0cc9e3dec3dd36d6ae3
mauvekeith <47022312+mauvekeith@users.noreply.github.com>
2019-03-07 19:55:53 +0800

    Update 192db.sql

--- 192db.sql
+++ 192db.sql
@@ -34,6 +34,7 @@ v1.1 - Jan 29, 2019 - Added INSERT INTO statements [Daine Daling]
 v1.2 - Feb 8, 2019 - Added comments and other code information
 v2.0 - Feb 20, 2019 - Added User View, added ID columns for Student and EstablishmentRepresentative
 v2.1 - Feb 21, 2019 - Added new entries, modified Establishment table (removed foreign key constraint)
+v2.2 - Mar 6. 2019 - Added more entries
 */
 
 /*
@@ -116,6 +117,10 @@ INSERT INTO Admin VALUES(1,"agacutan","Aly Gacutan","alyg@up.edu.ph","password")
 INSERT INTO Establishment VALUES(1,"Daine Store","NIGS, UP Diliman","Daine is cute","Grocery","24 hours","09454685149","",0,"ddaling");
 INSERT INTO Establishment VALUES(2,"Kenneth Store","DCS, UP Diliman","Kenneth Store offers affordable food","Restaurant","9am to 7pm","09154444321","",0,"ksantos");
 INSERT INTO Establishment VALUES(3,"Aly Store","Area 2, UP Diliman","Aly Store offers affordable printing and photocopy","printing, photocopy","8am to 5pm","09995551234","",0,"agacutan");
+INSERT INTO Establishment VALUES(6,"Blessings UPD","Shopping Center, UPD","Offers school and office supplies","supplies","8am to 6pm","09151684091","",0,"");
+INSERT INTO Establishment VALUES(7,"Ministop","Maginhawa St., Diliman","Convenience Store","food,supplies","24 hours","n/a","",0,"");
+INSERT INTO Establishment VALUES(8,"National Bookstore","Katipunan","Bookstore","supplies","7am to 8pm","n/a","",0,"");
+INSERT INTO Establishment VALUES(9,"Hello World Store","UP Diliman","Computer shop","internet,printing","24 hours","09190531221","",0,"");
 INSERT INTO EstablishmentRepresentative VALUES(2,"wmtan","KuyaWil","wmtan@dcs.upd.edu.ph","kuyawil","");
 INSERT INTO Establishment VALUES(4,"Wilson Pharmacy","Krus na Ligas, Diliman","Alternative medicine","Drugstore","24 hours","09166611666","",1,"wmtan");
 INSERT INTO Establishment VALUES(5,"Kuya Wil Supplies","Krus na Ligas, Diliman","Affordable school supplies","School Supplies","7am to 9pm","09166611666","",1,"wmtan");
